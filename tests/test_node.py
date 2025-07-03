18cb88045597dcf8e8469d1d1e54f8994598c7f6
Cyrile-Duquenoy <cyrileduquenoy@hotmail.fr>
2025-07-03 23:53:52 +0200

    Update test_node.py

--- tests/test_node.py
+++ tests/test_node.py
@@ -3,13 +3,6 @@ from FEM.core.node import Node
 
 class TestNode(unittest.TestCase):
     def test_player_creation(self):
-        '''
-        caracteristiques = Caracteristiques(PV=100, PM=50)
-        player = Player('Elyris', race=Race.HUMAIN, classe=Classe.PALADIN, caracteristiques=caracteristiques)
-        self.assertEqual(player._name, 'Elyris')
-        self.assertEqual(player._race, Race.HUMAIN)
-        self.assertEqual(player.caracteristiques['PV'], 100)
-        '''
         node = Node(1,0,2.3, ids = 32)
         self.assertEqual(node._x, 1)
 
